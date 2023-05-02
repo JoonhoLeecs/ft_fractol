ed42f27328825e98c078237868dad7225123e3d1
Joonho Lee <joonhlee@student.42seoul.kr>
2023-05-02 20:55:55 +0900

    add key and mouse hook

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: joonhlee <joonhlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 16:14:58 by joonhlee          #+#    #+#             */
-/*   Updated: 2023/05/01 21:01:05 by joonhlee         ###   ########.fr       */
+/*   Updated: 2023/05/02 20:44:46 by joonhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,69 +14,100 @@
 
 int	main(int argc, char **argv)
 {
-	int		set;
-	void	*mlx;
-	void	*mlx_win;
-	t_img	img;
+	t_param	param;
 
-	set = check_arg(argc, argv);
-	if (set < 0)
+	param.set = check_arg(&param, argc, argv);
+	if (param.set < 0)
 		return (print_arg_error());
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1600, 1600, "Fract'ol");
-	img.img = mlx_new_image(mlx, 1600, 1600);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.size_line,
-			&img.endian);
-	init_plane(&img, set, 1600, 1600);
-	set_img_color(&img, 1600, 1600);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	fractol_init(&param);
+	mlx_put_image_to_window(param.mlx, param.mlx_win, param.img.img, 0, 0);
+	mlx_hook(param.mlx_win, 17, 0L, &exit_on_destroy, &param);
+	mlx_key_hook(param.mlx_win, key_hook, &param);
+	mlx_mouse_hook(param.mlx_win, mouse_hook, &param);
+	mlx_loop(param.mlx);
+}
 
+int	check_arg(t_param *param, int argc, char **argv)
+{
+	int	arg;
 
+	if (!(argc >= 2 && argc <= 3))
+		return (-1);
+	if (ft_strcmp("Mandelbrot", argv[1]) == 0 && argc == 2)
+		return (0);
+	else if (ft_strcmp("Julia", argv[1]) == 0 && argc == 3)
+	{
+		arg = ft_atoi(argv[2]);
+		if (arg < 1 || arg > 4)
+			return (-1);
+		param->arg = arg;
+		return (1);
+	}
+	else
+		return (-1);
 }
 
 int	print_arg_error(void)
 {
 	ft_putstr_fd("Invalid argument\n", 1);
 	ft_putstr_fd("Mandelbrot set: Mandelbrot\n", 1);
-	ft_putstr_fd("Julia set: Julia\n", 1);
+	ft_putstr_fd("Julia set: Julia arg(1 - 4)\n", 1);
 	return (1);
 }
 
-int	check_arg(int argc, char **argv)
+void	fractol_init(t_param *param)
 {
-	if (argc != 2)
-		return (-1);
-	if (ft_strcmp("Mandelbrot", argv[1]) == 0)
-		return (0);
-	else if (ft_strcmp("Julia", argv[1]) == 0)
-		return (1);
-	else
-		return (-1);
+	param->mlx = mlx_init();
+	param->width = 620;
+	param->height = 560;
+	param->mlx_win = mlx_new_window(param->mlx, param->width, param->height,
+			"Fract'ol");
+	param->img.img = mlx_new_image(param->mlx, param->width, param->height);
+	param->img.addr = mlx_get_data_addr(param->img.img, &(param->img.bpp),
+			&(param->img.size_line), &(param->img.endian));
+	init_palette(param);
+	init_plane(param);
 }
 
-void	set_pixel_color(t_img *img, int x, int y, int color)
+void	init_plane(t_param *param)
 {
-	char	*dst;
-
-	// color = (color >= 0) * color + (color < 0) * (color + 2147483648);
-	dst = img->addr + y * img->size_line + x * img->bpp / 8;
-	*(unsigned int *)dst = color;
+	if (param->set == 0)
+	{
+		param->img.xmin = -2;
+		param->img.ymin = -1.12;
+		param->img.xinc = 2.47 / (double) param->width;
+		param->img.yinc = 2.24 / (double) param->height;
+		set_img_color(param, &param->img);
+	}
+	else if (param->set == 1)
+	{
+		param->img.xmin = -1.24;
+		param->img.ymin = -1.12;
+		param->img.xinc = 2.47 / (double) param->width;
+		param->img.yinc = 2.24 / (double) param->height;
+		set_img_color(param, &param->img);
+	}
 }
 
-void	set_img_color(t_img *img, int width, int height)
+void	set_img_color(t_param *param, t_img *img)
 {
 	int	x;
 	int	y;
 	int	color;
 
 	x = 0;
-	while (x < width)
+	while (x < param->width)
 	{
 		y = 0;
-		while (y < height)
+		while (y < param->height)
 		{
-			color = mandelbrot(x, y, img);
+			if (param->set == 0)
+				color = mandelbrot(x, y, img);
+			else if (param->set == 1)
+				color = julia(x, y, img, param->arg);
+			else
+				color = mandelbrot(x, y, img);
+			color = param->plt[param->pind](color);
 			set_pixel_color(img, x, y, color);
 			y++;
 		}
@@ -86,47 +117,339 @@ void	set_img_color(t_img *img, int width, int height)
 
 int	mandelbrot(int x, int y, t_img *img)
 {
-	int	iter;
-	double	cx;
-	double	cy;
-	double	zx;
-	double	zy;
-	double x2;
-	double y2;
-	double w;
+	int		iter;
+	t_mand	mand;
 
-	cx = img->xmin + (double)x * img->xinc;
-	cy = img->ymin + (double)y * img->yinc;
+	mand.cx = img->xmin + (double)x * img->xinc;
+	mand.cy = img->ymin + (double)y * img->yinc;
 	iter = 0;
-	zx = 0;
-	zy = 0;
-	x2 = 0;
-	y2 = 0;
-	w = 0;
-	while (x2 + y2 <= 4 && iter < 1023)
+	mand.zx = 0;
+	mand.zy = 0;
+	mand.x2 = 0;
+	mand.y2 = 0;
+	mand.w = 0;
+	while (mand.x2 + mand.y2 <= 4 && iter < 255)
 	{
-		zx = x2 - y2 + cx;
-		zy = w + cy;
-		x2 = zx * zx;
-		y2 = zy * zy;
-		w = 2 * zx * zy;
+		mand.zx = mand.x2 - mand.y2 + mand.cx;
+		mand.zy = mand.w + mand.cy;
+		mand.x2 = mand.zx * mand.zx;
+		mand.y2 = mand.zy * mand.zy;
+		mand.w = 2 * mand.zx * mand.zy;
 		iter++;
 	}
-	// printf("iter:%d|", iter);
-	// printf("color:%d\n", (iter / 4) | (iter / 4) << 8 | (iter / 4) << 16);
-	return (255 - (iter / 4));
-	// return ((iter / 4) | (iter / 4) << 8 | (iter / 4) << 16);
+	return (255 - iter);
 }
 
-void	init_plane(t_img *img, int set, int width, int height)
+int	julia(int x, int y, t_img *img, int arg)
 {
-	if (set != 0)
-		return ;
-	img->xmin = -2.5;
-	img->ymin = -1;
-	img->xinc = 3.5 / (double) width;
-	img->yinc = 2 / (double) height;
+	int		iter;
+	t_mand	julia;
 
-	// printf("%f, %f, %f, %f\n", img->xmin, img->xinc, img->ymin, img->yinc);
+	julia.cx = julia_preset('x', arg);
+	julia.cy = julia_preset('y', arg);
+	iter = 0;
+	julia.zx = img->xmin + (double)x * img->xinc;
+	julia.zy = img->ymin + (double)y * img->yinc;
+	julia.x2 = julia.zx * julia.zx;
+	julia.y2 = julia.zy * julia.zy;
+	julia.w = 2 * julia.zx * julia.zy;
+	while (julia.x2 + julia.y2 <= 4 && iter < 255)
+	{
+		julia.zx = julia.x2 - julia.y2 + julia.cx;
+		julia.zy = julia.w + julia.cy;
+		julia.x2 = julia.zx * julia.zx;
+		julia.y2 = julia.zy * julia.zy;
+		julia.w = 2 * julia.zx * julia.zy;
+		iter++;
+	}
+	return (255 - iter);
 }
 
+double	julia_preset(char c, int arg)
+{
+	if (arg == 1 && c == 'x')
+		return (-0.4);
+	else if (arg == 1 && c == 'y')
+		return (0.6);
+	else if (arg == 2 && c == 'x')
+		return (0.285);
+	else if (arg == 2 && c == 'y')
+		return (0.01);
+	else if (arg == 3 && c == 'x')
+		return (-0.8);
+	else if (arg == 3 && c == 'y')
+		return (-0.156);
+	else if (arg == 4 && c == 'x')
+		return (-0.7269);
+	else if (arg == 4 && c == 'y')
+		return (0.1889);
+	else
+		return (4);
+}
+
+void	set_pixel_color(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->addr + y * img->size_line + x * img->bpp / 8;
+	*(unsigned int *)dst = color;
+}
+
+int	key_hook(int keycode, t_param *param)
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_window(param->mlx, param->mlx_win);
+		exit (0);
+	}
+	if (keycode == 126 || keycode == 125)
+		vertical_move(param, keycode);
+	if (keycode == 123 || keycode == 124)
+		horizon_move(param, keycode);
+	if (keycode == 1)
+		new_palette(param);
+	if (keycode == 2)
+		new_arg(param);
+	return (0);
+}
+
+int	exit_on_destroy(int keycode, t_param *param)
+{
+	int	tmp;
+
+	tmp = keycode;
+	tmp = param->width;
+	exit(0);
+}
+
+int	mouse_hook(int button, int x, int y, t_param *param)
+{
+	int	i;
+
+	i = x + y;
+	if (button == 4)
+		zoom_out(param, x, y);
+	if (button == 5)
+		zoom_in(param, x, y);
+	return (0);
+}
+
+void	zoom_in(t_param *param, int x, int y)
+{
+	param->temp_img.img = mlx_new_image(param->mlx, param->width,
+			param->height);
+	param->temp_img.addr = mlx_get_data_addr(param->temp_img.img,
+			&(param->temp_img.bpp), &(param->temp_img.size_line),
+			&(param->temp_img.endian));
+	param->temp_img.xmin = param->img.xmin + x * param->img.xinc * 0.1;
+	param->temp_img.xinc = param->img.xinc * 0.9;
+	param->temp_img.ymin = param->img.ymin + (param->height - y - 1) * param->img.yinc * 0.1;
+	param->temp_img.yinc = param->img.yinc * 0.9;
+	set_img_color(param, &param->temp_img);
+	mlx_put_image_to_window(param->mlx, param->mlx_win, param->temp_img.img, 0, 0);
+	clear_prev_img(param);
+}
+
+void	zoom_out(t_param *param, int x, int y)
+{
+	param->temp_img.img = mlx_new_image(param->mlx, param->width,
+			param->height);
+	param->temp_img.addr = mlx_get_data_addr(param->temp_img.img,
+			&(param->temp_img.bpp), &(param->temp_img.size_line),
+			&(param->temp_img.endian));
+	param->temp_img.xmin = param->img.xmin - x * param->img.xinc * 0.1;
+	param->temp_img.xinc = param->img.xinc * 1.1;
+	param->temp_img.ymin = param->img.ymin - (param->height - y - 1) * param->img.yinc * 0.1;
+	param->temp_img.yinc = param->img.yinc * 1.1;
+	// param->temp_img.xmin = param->img.xmin * 1.1;
+	// param->temp_img.xinc = param->img.xinc * 1.1;
+	// param->temp_img.ymin = param->img.ymin * 1.1;
+	// param->temp_img.yinc = param->img.yinc * 1.1;
+	set_img_color(param, &param->temp_img);
+	mlx_put_image_to_window(param->mlx, param->mlx_win, param->temp_img.img, 0, 0);
+	clear_prev_img(param);
+}
+
+void	clear_prev_img(t_param *param)
+{
+	mlx_destroy_image(param->mlx, param->img.img);
+	param->img.img = param->temp_img.img;
+	param->img.addr = param->temp_img.addr;
+	param->img.bpp = param->temp_img.bpp;
+	param->img.size_line = param->temp_img.size_line;
+	param->img.endian = param->temp_img.endian;
+	param->img.xmin = param->temp_img.xmin;
+	param->img.xinc = param->temp_img.xinc;
+	param->img.ymin = param->temp_img.ymin;
+	param->img.yinc = param->temp_img.yinc;
+}
+
+void	vertical_move(t_param *param, int key)
+{
+	int	direction;
+
+	direction = -2 * (key - 126) - 1;
+	param->temp_img.img = mlx_new_image(param->mlx, param->width,
+			param->height);
+	param->temp_img.addr = mlx_get_data_addr(param->temp_img.img,
+			&(param->temp_img.bpp), &(param->temp_img.size_line),
+			&(param->temp_img.endian));
+	param->temp_img.xmin = param->img.xmin;
+	param->temp_img.xinc = param->img.xinc;
+	param->temp_img.ymin = param->img.ymin
+		+ direction * param->img.yinc * (param->height / 20);
+	param->temp_img.yinc = param->img.yinc;
+	// if (direction > 0)
+	// 	vm_set_img_up(param, &param->temp_img, &param->img);
+	// else
+	// 	vm_set_img_down(param, &param->temp_img, &param->img);
+	set_img_color(param, &param->temp_img);
+	mlx_put_image_to_window(param->mlx, param->mlx_win, param->temp_img.img, 0, 0);
+	clear_prev_img(param);
+}
+
+void	horizon_move(t_param *param, int key)
+{
+	int	direction;
+
+	direction = 2 * (key - 123) - 1;
+	param->temp_img.img = mlx_new_image(param->mlx, param->width,
+			param->height);
+	param->temp_img.addr = mlx_get_data_addr(param->temp_img.img,
+			&(param->temp_img.bpp), &(param->temp_img.size_line),
+			&(param->temp_img.endian));
+	param->temp_img.xmin = param->img.xmin
+		+ direction * param->img.xinc * (param->width / 20);
+	param->temp_img.xinc = param->img.xinc;
+	param->temp_img.ymin = param->img.ymin;
+	param->temp_img.yinc = param->img.yinc;
+	// if (direction > 0)
+	// 	vm_set_img_up(param, &param->temp_img, &param->img);
+	// else
+	// 	vm_set_img_down(param, &param->temp_img, &param->img);
+	set_img_color(param, &param->temp_img);
+	mlx_put_image_to_window(param->mlx, param->mlx_win, param->temp_img.img, 0, 0);
+	clear_prev_img(param);
+}
+
+void	new_palette(t_param *param)
+{
+	param->pind = (param->pind + 1) % 7;
+	param->temp_img.img = mlx_new_image(param->mlx, param->width,
+			param->height);
+	param->temp_img.addr = mlx_get_data_addr(param->temp_img.img,
+			&(param->temp_img.bpp), &(param->temp_img.size_line),
+			&(param->temp_img.endian));
+	param->temp_img.xmin = param->img.xmin;
+	param->temp_img.xinc = param->img.xinc;
+	param->temp_img.ymin = param->img.ymin;
+	param->temp_img.yinc = param->img.yinc;
+	// if (direction > 0)
+	// 	vm_set_img_up(param, &param->temp_img, &param->img);
+	// else
+	// 	vm_set_img_down(param, &param->temp_img, &param->img);
+	set_img_color(param, &param->temp_img);
+	mlx_put_image_to_window(param->mlx, param->mlx_win, param->temp_img.img, 0, 0);
+	clear_prev_img(param);
+}
+
+void	new_arg(t_param *param)
+{
+	if (param->arg < 4)
+		param->arg = param->arg + 1;
+	else
+		param->arg = 1;
+	param->temp_img.img = mlx_new_image(param->mlx, param->width,
+			param->height);
+	param->temp_img.addr = mlx_get_data_addr(param->temp_img.img,
+			&(param->temp_img.bpp), &(param->temp_img.size_line),
+			&(param->temp_img.endian));
+	param->temp_img.xmin = param->img.xmin;
+	param->temp_img.xinc = param->img.xinc;
+	param->temp_img.ymin = param->img.ymin;
+	param->temp_img.yinc = param->img.yinc;
+	// if (direction > 0)
+	// 	vm_set_img_up(param, &param->temp_img, &param->img);
+	// else
+	// 	vm_set_img_down(param, &param->temp_img, &param->img);
+	set_img_color(param, &param->temp_img);
+	mlx_put_image_to_window(param->mlx, param->mlx_win, param->temp_img.img, 0, 0);
+	clear_prev_img(param);
+}
+
+void	init_palette(t_param *param)
+{
+	param->plt[0] = &palette1;
+	param->plt[1] = &palette2;
+	param->plt[2] = &palette3;
+	param->plt[3] = &palette4;
+	param->plt[4] = &palette5;
+	param->plt[5] = &palette6;
+	param->plt[6] = &palette7;
+	param->pind = 0;
+}
+
+int	palette1(int color)
+{
+	return (color);
+}
+
+int	palette2(int color)
+{
+	return (color << 8);
+}
+
+int	palette3(int color)
+{
+	return (color << 8 | color);
+}
+
+int	palette4(int color)
+{
+	return (color << 16);
+}
+
+int	palette5(int color)
+{
+	return (color << 16 | color);
+}
+
+int	palette6(int color)
+{
+	return (color << 16 | color << 8);
+}
+
+int	palette7(int color)
+{
+	return (color << 16 | color << 8 | color);
+}
+
+
+
+// void	vm_set_img_up(t_param *param, t_img *tmp, t_img *img, int dir)
+// {
+// 	int	x;
+// 	int	y;
+// 	int	color;
+
+// 	x = 0;
+// 	while (x < param->width)
+// 	{
+// 		y = param->height - 1 ;
+// 		// while (y >= param->height * 0.1)
+// 		while (y >= 0)
+// 		{
+// 			if (y >= param->height *)
+// 			y--;
+// 		}
+// 		{
+// 			if (param->set == 0)
+// 				color = mandelbrot(x, y, img);
+// 			else
+// 				color = mandelbrot(x, y, img);
+// 			set_pixel_color(img, x, y, color);
+// 			y--;
+// 		}
+// 		x++;
+// 	}
+// }
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: joonhlee <joonhlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 16:15:11 by joonhlee          #+#    #+#             */
-/*   Updated: 2023/05/01 20:32:37 by joonhlee         ###   ########.fr       */
+/*   Updated: 2023/05/02 20:41:35 by joonhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,59 @@ typedef struct s_img
 
 }			t_img;
 
-int check_arg(int argc, char **argv);
+typedef struct s_param
+{
+	int		set;
+	int		arg;
+	void	*mlx;
+	void	*mlx_win;
+	int		width;
+	int		height;
+	t_img	img;
+	t_img	temp_img;
+	int		(*plt[7])(int);
+	int		pind;
+}			t_param;
+
+typedef struct s_mand
+{
+	double	cx;
+	double	cy;
+	double	zx;
+	double	zy;
+	double	x2;
+	double	y2;
+	double	w;
+}			t_mand;
+
+// int check_arg(int argc, char **argv);
+int check_arg(t_param *param, int argc, char **argv);
 int ft_strcmp(char *s1, char *s2);
 int print_arg_error(void);
+void fractol_init(t_param *param);
 void set_pixel_color(t_img *img, int x, int y, int color);
-void set_img_color(t_img *img, int width, int height);
 int mandelbrot(int x, int y, t_img *img);
-void init_plane(t_img *img, int set, int width, int height);
+int julia(int x, int y, t_img *img, int arg);
+double julia_preset(char c, int arg);
+void init_plane(t_param *param);
+void set_img_color(t_param *param, t_img *img);
+int key_hook(int keycode, t_param *param);
+int exit_on_destroy(int keycode, t_param *param);
+int mouse_hook(int button, int x, int y, t_param *param);
+void zoom_in(t_param *param, int x, int y);
+void zoom_out(t_param *param, int x, int y);
+void clear_prev_img(t_param *param);
+void vertical_move(t_param *param, int key);
+void horizon_move(t_param *param, int key);
+void init_palette(t_param *param);
+int palette1(int color);
+int palette2(int color);
+int palette3(int color);
+int palette4(int color);
+int palette5(int color);
+int palette6(int color);
+int palette7(int color);
+void new_palette(t_param *param);
+void new_arg(t_param *param);
 
 #endif
